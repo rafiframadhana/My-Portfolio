2c229c2faed18a2ed82d863b2af5e4e5f413cd86
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-01-19 01:59:54 +0700

    Update certifications

--- scripts/certification.js
+++ scripts/certification.js
@@ -1,24 +1,30 @@
 const certifications = [
     {
         img: "images/certifications/certification-1.png",
+        title: "JavaScript DSA",
+        issuer: "Issued by freeCodeCamp",
+        link: "https://freecodecamp.org/certification/fccc37dcbb4-6eb7-444e-afc3-a726f368d480/javascript-algorithms-and-data-structures-v8"
+    },
+    {
+        img: "images/certifications/certification-2.png",
         title: "Responsive Web Design",
         issuer: "Issued by freeCodeCamp",
         link: "https://freecodecamp.org/certification/fccc37dcbb4-6eb7-444e-afc3-a726f368d480/responsive-web-design"
     },
     {
-        img: "images/certifications/certification-2.png",
+        img: "images/certifications/certification-3.png",
         title: "JavaScript Certificate",
         issuer: "Issued by HackerRank",
         link: "https://www.hackerrank.com/certificates/iframe/63bd5470a0b7"
     },
     {
-        img: "images/certifications/certification-3.png",
+        img: "images/certifications/certification-4.png",
         title: "SQL Certificate",
         issuer: "Issued by HackerRank",
         link: "https://www.hackerrank.com/certificates/iframe/caf7f45d08e6"
     },
     {
-        img: "images/certifications/certification-4.png",
+        img: "images/certifications/certification-5.png",
         title: "CSS Certificate",
         issuer: "Issued by HackerRank",
         link: "https://www.hackerrank.com/certificates/iframe/5214ad063526"
