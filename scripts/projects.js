b5babc608a9b68ec484688239bb12ae937e86452
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-02-24 21:22:42 +0700

    Update projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,77 +1,102 @@
 const projects = [
-    { 
-        img: "images/projects/project-1.png", 
-        title: "Amazon Clone", 
-        description: "A replica of Amazon's online store, showcasing product listings, a shopping cart, and a checkout process.", 
-        link: "https://rafiframadhana.github.io/amazon-clone/" 
+    {
+        img: "images/projects/project-1.png",
+        title: "Amazon Clone",
+        description: "A replica of Amazon's online store, showcasing product listings, a shopping cart, and a checkout process.",
+        link: "https://rafiframadhana.github.io/amazon-clone/"
     },
-    { 
-        img: "images/projects/project-2.png", 
-        title: "Weather App", 
-        description: "A weather web application that allows users to check the current weather by searching for a specific city.", 
-        link: "https://rafiframadhana.github.io/Weather-App/" 
+    {
+        img: "images/projects/project-2.png",
+        title: "RecipeBot AI",
+        description: "An AI-powered recipe suggestion app that helps users discover new recipes based on available ingredients.",
+        link: "https://recipebot-ai.netlify.app/"
     },
-    { 
-        img: "images/projects/project-3.png", 
-        title: "Rock Paper Scissors", 
-        description: "A classic Rock, Paper, Scissors game where users can play against the computer.", 
-        link: "https://rafiframadhana.github.io/Rock-Paper-Scissors-Game/" 
+    {
+        img: "images/projects/project-3.png",
+        title: "Weather App",
+        description: "A weather web application that allows users to check the current weather by searching for a specific city.",
+        link: "https://rafiframadhana.github.io/Weather-App/"
     },
-    { 
-        img: "images/projects/project-4.png", 
-        title: "Clothing Store", 
-        description: "A basic online clothing store for Bray Surfing, showcasing product listings and allowing users to browse through available items.", 
-        link: "https://rafiframadhana.github.io/Bray-Surfing-Clothing-Store/" 
+    {
+        img: "images/projects/project-4.png",
+        title: "Travel Journal",
+        description: "A digital travel journal where users can document their trips, upload photos, and share experiences.",
+        link: "https://rafiframadhana.github.io/travel-journal/"
     },
-    { 
-        img: "images/projects/project-5.png", 
-        title: "Calculator", 
-        description: "A simple calculator that supports basic arithmetic operations like addition, subtraction, multiplication, and division.", 
-        link: "https://rafiframadhana.github.io/Calculator/" 
+    {
+        img: "images/projects/project-5.png",
+        title: "Clothing Store",
+        description: "A basic online clothing store for Bray Surfing, showcasing product listings and allowing users to browse through available items.",
+        link: "https://rafiframadhana.github.io/Bray-Surfing-Clothing-Store/"
+        
     },
-    { 
-        img: "images/projects/project-6.png", 
-        title: "To do List", 
-        description: "A to-do list application that allows users to add, edit, and delete tasks to stay organized.", 
-        link: "https://rafiframadhana.github.io/To-do-List/" 
+    {
+        img: "images/projects/project-6.png",
+        title: "Youtube Clone",
+        description: "A simple YouTube clone, featuring a basic layout with video thumbnails, titles, and a functional video player.",
+        link: "https://rafiframadhana.github.io/youtube-clone/"
     },
-    { 
-        img: "images/projects/project-7.png", 
-        title: "Number Guessing Game", 
-        description: "A game where the player guesses a randomly generated number, with feedback on whether the guess is too high or low.", 
-        link: "https://rafiframadhana.github.io/Number-Guessing-Game/" 
+    {
+        img: "images/projects/project-7.png",
+        title: "Word Guessing Game",
+        description: "A classic word-guessing game where players attempt to guess the hidden word within a limited number of tries.",
+        link: "https://rafiframadhana.github.io/assembly-endgame/"
     },
-    { 
-        img: "images/projects/project-8.png", 
-        title: "Password Generator", 
-        description: "Generates customizable random passwords with options for length and character types (letters, numbers, symbols).", 
-        link: "https://rafiframadhana.github.io/Password-Generator/" 
+    {
+        img: "images/projects/project-8.png",
+        title: "Tenzies",
+        description: "A fun and interactive dice-rolling game where players try to get all dice to show the same number.",
+        link: "https://rafiframadhana.github.io/tenzies-game/"
     },
-    { 
-        img: "images/projects/project-9.png", 
-        title: "Temperature Conversion", 
-        description: "Converts temperatures between Celsius, Fahrenheit, and Kelvin.", 
-        link: "https://rafiframadhana.github.io/Temperature-conversion/" 
+    {
+        img: "images/projects/project-9.png",
+        title: "Calculator",
+        description: "A simple calculator that supports basic arithmetic operations like addition, subtraction, multiplication, and division.",
+        link: "https://rafiframadhana.github.io/Calculator/"
     },
-    { 
-        img: "images/projects/project-10.png", 
-        title: "Dice Roller Game", 
-        description: "Simulates rolling one or more dice and displays the results visually.", 
-        link: "https://rafiframadhana.github.io/Dice-Roller-Game/" 
+    {
+        img: "images/projects/project-10.png",
+        title: "To do List",
+        description: "A to-do list application that allows users to add, edit, and delete tasks to stay organized.",
+        link: "https://rafiframadhana.github.io/To-do-List/"
     },
-    { 
-        img: "images/projects/project-11.png", 
-        title: "Stopwatch", 
-        description: "A minimalist stopwatch with start, stop, and reset functions, designed with a modern interface.", 
-        link: "https://rafiframadhana.github.io/Stopwatch/" 
+    {
+        img: "images/projects/project-11.png",
+        title: "Rock Paper Scissors",
+        description: "A classic Rock, Paper, Scissors game where users can play against the computer.",
+        link: "https://rafiframadhana.github.io/Rock-Paper-Scissors-Game/"
     },
-    { 
-        img: "images/projects/project-12.png", 
-        title: "Youtube Clone", 
-        description: "A simple YouTube clone, featuring a basic layout with video thumbnails, titles, and a functional video player.", 
-        link: "https://rafiframadhana.github.io/youtube-clone/" 
+    {
+        img: "images/projects/project-12.png",
+        title: "Password Generator",
+        description: "Generates customizable random passwords with options for length and character types (letters, numbers, symbols).",
+        link: "https://rafiframadhana.github.io/Password-Generator/"
     },
-    
+    {
+        img: "images/projects/project-13.png",
+        title: "Temperature Conversion",
+        description: "Converts temperatures between Celsius, Fahrenheit, and Kelvin.",
+        link: "https://rafiframadhana.github.io/Temperature-conversion/"
+    },
+    {
+        img: "images/projects/project-14.png",
+        title: "Dice Roller Game",
+        description: "Simulates rolling one or more dice and displays the results visually.",
+        link: "https://rafiframadhana.github.io/Dice-Roller-Game/"
+    },
+    {
+        img: "images/projects/project-15.png",
+        title: "Stopwatch",
+        description: "A minimalist stopwatch with start, stop, and reset functions, designed with a modern interface.",
+        link: "https://rafiframadhana.github.io/Stopwatch/"
+    },
+    {
+        img: "images/projects/project-16.png",
+        title: "Number Guessing Game",
+        description: "A game where the player guesses a randomly generated number, with feedback on whether the guess is too high or low.",
+        link: "https://rafiframadhana.github.io/Number-Guessing-Game/"
+    },
+
 ];
 
 
