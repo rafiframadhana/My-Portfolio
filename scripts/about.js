d0b15200fdea4f72ee97a52af67313c3fadb2eb1
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-03-19 07:47:21 +0700

    Add new items to skills section

--- scripts/about.js
+++ scripts/about.js
@@ -1,16 +1,28 @@
 const skills = [
-    { name: "React JS", icon: "images/icon/react-js.png" },
+    { name: "Next.js", icon: "images/icon/nextjs.png" },
+    { name: "React", icon: "images/icon/react.png" },
     { name: "JavaScript", icon: "images/icon/js.png" },
     { name: "HTML5", icon: "images/icon/html.png" },
     { name: "CSS3", icon: "images/icon/css.png" },
     { name: "SQL (MySQL)", icon: "images/icon/sql.png" },
-    { name: "Responsive Web Design", icon: "images/icon/responsive-web.png" },
+    { name: "Redux Toolkit", icon: "images/icon/redux-toolkit.png" },
+    { name: "Zustand", icon: "images/icon/zustand.png" },
+    { name: "React Router", icon: "images/icon/react-router.png" },
+    { name: "Tailwind CSS", icon: "images/icon/tailwind-css.png" },
+    { name: "Material UI", icon: "images/icon/material-ui.png" },
+    { name: "Chakra UI", icon: "images/icon/chakra-ui.png" },
+    { name: "Vercel", icon: "images/icon/vercel.png" },
+    { name: "Netlify", icon: "images/icon/netlify.png" },
+    { name: "Vite", icon: "images/icon/vite.png" },
+    { name: "npm", icon: "images/icon/npm.png" },
+    { name: "ESLint", icon: "images/icon/eslint.png" },
+    { name: "Prettier", icon: "images/icon/prettier.png" },
     { name: "API Integration", icon: "images/icon/api.png" },
-    { name: "Jasmine Framework", icon: "images/icon/jasmine.png" },
+    { name: "Jasmine", icon: "images/icon/jasmine.png" },
     { name: "Git", icon: "images/icon/git.png" },
     { name: "GitHub", icon: "images/icon/github-icon.png" },
     { name: "VS Code", icon: "images/icon/vscode.png" },
-    { name: "Chrome DevTools", icon: "images/icon/chrome-dev-tools.png" }
+    { name: "Figma", icon: "images/icon/figma.png" },
 ];
 
 
